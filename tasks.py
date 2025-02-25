34f53193060f7d72bdd309ff860d8c425e1920ca
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-02-25 08:24:24 -0800

    add client to column

--- tasks.py
+++ tasks.py
@@ -7,7 +7,8 @@ COST_DIR = "./costs"
 
 
 @task
-def analyze_account_data(_):
+def analyze_account_data(_, head: str = "3"):
+    head = int(head)
 
     for file_name in [f for f in listdir(COST_DIR)]:
         df: DataFrame = pd.read_csv(f"{COST_DIR}/{file_name}", index_col=False)
@@ -19,14 +20,17 @@ def analyze_account_data(_):
         df_top3 = (
             df.sort_values(["Month", "Cost"], ascending=[True, False])
             .groupby("Month")
-            .head(3)
+            .head(head)
         )
 
+        df_top3[f"Top {head} Costs"] = df_top3["Service"] + ": " + df_top3["Cost"].astype(str)
+        df = df_top3[["Month", f"Top {head} Costs"]]
+        df_grouped = df.groupby("Month")[f"Top {head} Costs"].agg(list).reset_index()
+        df_expanded = df_grouped[f"Top {head} Costs"].apply(lambda x: pd.Series(x[:head]))
+        df_expanded.columns = ["1", "2", "3"]
+        df_final = pd.concat([df_grouped["Month"], df_expanded], axis=1)
+        df_final.insert(0, 'client', file_name.split('.csv')[0])
+
         print(f"----------------- {file_name.split('.csv')[0]} -----------------")
-
-        df_top3["Top 3 Costs"] = df_top3["Service"] + ": " + df_top3["Cost"].astype(str)
-        df = df_top3[["Month", "Top 3 Costs"]]
-
-        print(df.groupby("Month")["Top 3 Costs"].agg(", ".join).reset_index())
-
+        print(df_final)
         print("\n")
