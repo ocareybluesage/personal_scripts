42eb2014a1a507aacd41a274c6e3a20f1d26304e
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-02-25 08:41:37 -0800

    write output to csv file

--- tasks.py
+++ tasks.py
@@ -9,6 +9,9 @@ COST_DIR = "./costs"
 @task
 def analyze_account_data(_, head: str = "3"):
     head = int(head)
+    top_columns = [str(i) for i in range(1, head + 1)]
+
+    output: DataFrame = pd.DataFrame(columns=['client','Month'] + top_columns)
 
     for file_name in [f for f in listdir(COST_DIR)]:
         df: DataFrame = pd.read_csv(f"{COST_DIR}/{file_name}", index_col=False)
@@ -17,20 +20,27 @@ def analyze_account_data(_, head: str = "3"):
         df = df.rename(columns={"Service": "Month"})
         df = df.melt(id_vars=["Month"], var_name="Service", value_name="Cost")
 
-        df_top3 = (
+        df_top = (
             df.sort_values(["Month", "Cost"], ascending=[True, False])
             .groupby("Month")
             .head(head)
         )
 
-        df_top3[f"Top {head} Costs"] = df_top3["Service"] + ": " + df_top3["Cost"].astype(str)
-        df = df_top3[["Month", f"Top {head} Costs"]]
+        df_top[f"Top {head} Costs"] = df_top["Service"] + ": " + df_top["Cost"].astype(str)
+        df = df_top[["Month", f"Top {head} Costs"]]
+
         df_grouped = df.groupby("Month")[f"Top {head} Costs"].agg(list).reset_index()
         df_expanded = df_grouped[f"Top {head} Costs"].apply(lambda x: pd.Series(x[:head]))
-        df_expanded.columns = ["1", "2", "3"]
+
+        df_expanded.columns = top_columns
+
         df_final = pd.concat([df_grouped["Month"], df_expanded], axis=1)
         df_final.insert(0, 'client', file_name.split('.csv')[0])
 
         print(f"----------------- {file_name.split('.csv')[0]} -----------------")
         print(df_final)
         print("\n")
+
+        output = pd.concat([output, df_final], ignore_index=True)
+
+    output.to_csv("output.csv", index=False)
